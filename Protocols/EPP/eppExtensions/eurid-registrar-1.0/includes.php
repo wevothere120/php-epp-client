a62f0defabf5c3140dbfe6dea46be438cf20d2a7
Metaregistrar <ewout@metaregistrar.com>
2016-10-03 14:51:53 +0200

    EURID registrar-1.0 error fixed

---
+++ Protocols/EPP/eppExtensions/eurid-registrar-1.0/includes.php
@@ -0,0 +1,2 @@
+<?php
+$this->addExtension('registrar', 'http://www.eurid.eu/xml/epp/registrar-1.0');
--- Registries/euridEppConnection/eppConnection.php
+++ Registries/euridEppConnection/eppConnection.php
@@ -8,7 +8,7 @@ class euridEppConnection extends eppConnection {
         //parent::enableDnssec();
         parent::setServices(array('urn:ietf:params:xml:ns:domain-1.0' => 'domain', 'urn:ietf:params:xml:ns:contact-1.0' => 'contact'));
         //parent::useExtension('nsgroup-1.1');
-        parent::useExtension('registrar-1.0');
+        parent::useExtension('eurid-registrar-1.0');
         parent::useExtension('authInfo-1.0');
         #parent::addCommandResponse('euridEppCreateRequest', 'euridEppCreateResponse');
         #parent::addCommandResponse('euridEppAuthcodeRequest', 'eppResponse');
