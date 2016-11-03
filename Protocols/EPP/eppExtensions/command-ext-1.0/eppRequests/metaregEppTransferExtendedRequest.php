4dada22d21dfe7709c269802be3f868d68cd73d1
Metaregistrar <ewout@metaregistrar.com>
2016-11-03 10:42:04 +0100

    Merge branch 'master' into namespaces-in-root-or-not
    # Conflicts:
    #	Protocols/EPP/eppExtensions/command-ext-1.0/eppRequests/metaregEppTransferExtendedRequest.php

--- Protocols/EPP/eppExtensions/command-ext-1.0/eppRequests/metaregEppTransferExtendedRequest.php
+++ Protocols/EPP/eppExtensions/command-ext-1.0/eppRequests/metaregEppTransferExtendedRequest.php
@@ -25,28 +25,27 @@ class metaregEppTransferExtendedRequest extends eppTransferRequest
 
         $transfer = $this->createElement('command-ext-domain:transfer');
         $ns = $this->createElement('command-ext-domain:ns');
-        $nameservers = $object->getHosts();
-        foreach ($nameservers as $nsRecord) {
-            /* @var eppHost $nsRecord */
-            $hostObj = $this->createElement('command-ext-domain:hostObj', $nsRecord->getHostname());
-            $ns->appendChild($hostObj);
-            /**
-             * @var eppHost $nsRecord
-             */
-            if ($nsRecord->getHostname()) {
-                $hostObj = $this->createElement('command-ext-domain:hostObj', $nsRecord->getHostname());
-                $ns->appendChild($hostObj);
-            } else {
-                throw new eppException("nsRecord has no hostname on metaregEppTransferExtendedRequest");
+        if (count($object->getHosts())>0) {
+            $nameservers = $object->getHosts();
+            foreach ($nameservers as $nsRecord) {
+                /**
+                 * @var eppHost $nsRecord
+                 */
+                if ($nsRecord->getHostname()) {
+                    $hostObj = $this->createElement('command-ext-domain:hostObj', $nsRecord->getHostname());
+                    $ns->appendChild($hostObj);
+                } else {
+                    throw new eppException("nsRecord has no hostname on metaregEppTransferExtendedRequest");
+                }
             }
+            $transfer->appendChild($ns);
         }
-        $transfer->appendChild($ns);
         if ($object->getRegistrant()) {
             $registrant = $this->createElement('command-ext-domain:registrant', $object->getRegistrant());
+            $transfer->appendChild($registrant);
         } else {
             throw new eppException("eppDomain object has no registrant on metaregEppTransferExtendedRequest");
         }
-        $transfer->appendChild($registrant);
         $types = ['admin', 'tech', 'billing'];
         foreach ($types as $type) {
             if ($object->getContact($type)) {
@@ -57,8 +56,8 @@ class metaregEppTransferExtendedRequest extends eppTransferRequest
                 throw new eppException("eppDomain object has no contact of type $type on metaregEppTransferExtendedRequest");
             }
         }
-        $domainChild->appendChild($transfer);
 
+        $domainChild->appendChild($transfer);
         $this->getExtension()->appendChild($commandExt);
         $this->addSessionId();
     }
