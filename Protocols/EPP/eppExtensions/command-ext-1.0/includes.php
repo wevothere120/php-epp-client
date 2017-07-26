49435e56f6b95cbc812ddc008a0bac021ab0136e
Ewout de Graaf <ewout@metaregistrar.com>
2017-07-26 16:26:57 +0200

    Merge branch 'master' of https://github.com/metaregistrar/php-epp-client into namespaces-in-root-or-not
    # Conflicts:
    #	Protocols/EPP/eppConnection.php

--- Protocols/EPP/eppExtensions/command-ext-1.0/includes.php
+++ Protocols/EPP/eppExtensions/command-ext-1.0/includes.php
@@ -1,7 +1,17 @@
 <?php
 include_once(dirname(__FILE__) . '/eppData/metaregInfoDomainOptions.php');
+
 include_once(dirname(__FILE__) . '/eppRequests/metaregInfoDomainRequest.php');
+$this->addCommandResponse('Metaregistrar\EPP\metaregInfoDomainRequest', 'Metaregistrar\EPP\eppInfoDomainResponse');
+
 include_once(dirname(__FILE__) . '/eppRequests/metaregEppAuthcodeRequest.php');
+$this->addCommandResponse('Metaregistrar\EPP\metaregEppAuthcodeRequest', 'Metaregistrar\EPP\eppInfoDomainResponse');
+
 include_once(dirname(__FILE__) . '/eppRequests/metaregEppPrivacyRequest.php');
+$this->addCommandResponse('Metaregistrar\EPP\metaregEppPrivacyRequest', 'Metaregistrar\EPP\eppUpdateDomainResponse');
+
 include_once(dirname(__FILE__) . '/eppRequests/metaregEppAutorenewRequest.php');
+$this->addCommandResponse('Metaregistrar\EPP\metaregEppAutorenewRequest', 'Metaregistrar\EPP\eppUpdateDomainResponse');
+
 include_once(dirname(__FILE__) . '/eppRequests/metaregEppTransferExtendedRequest.php');
+$this->addCommandResponse('Metaregistrar\EPP\metaregEppTransferExtendedRequest', 'Metaregistrar\EPP\eppTransferResponse');
