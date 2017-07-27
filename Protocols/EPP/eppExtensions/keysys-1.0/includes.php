6dee36ae6365cd84602f8cd20671848bc43569fc
Ewout de Graaf <ewout@metaregistrar.com>
2017-07-27 10:59:05 +0200

    Merge branch 'master' into namespaces-in-root-or-not
    # Conflicts:
    #	Registries/rrpproxyEppConnection/eppConnection.php

--- Protocols/EPP/eppExtensions/keysys-1.0/includes.php
+++ Protocols/EPP/eppExtensions/keysys-1.0/includes.php
@@ -3,3 +3,9 @@ $this->addExtension('keysys','http://www.key-systems.net/epp/keysys-1.0');
 
 include_once(dirname(__FILE__) . '/eppRequests/rrpproxyEppUpdateDomainRequest.php');
 $this->addCommandResponse('Metaregistrar\EPP\rrpproxyEppUpdateDomainRequest', 'Metaregistrar\EPP\eppUpdateDomainResponse');
+
+include_once(dirname(__FILE__) . '/eppRequests/rrpproxyEppWhoisPrivacyRequest.php');
+$this->addCommandResponse('Metaregistrar\EPP\rrpproxyEppWhoisPrivacyRequest', 'Metaregistrar\EPP\eppUpdateDomainResponse');
+
+include_once(dirname(__FILE__) . '/eppRequests/rrpproxyEppTrusteeRequest.php');
+$this->addCommandResponse('Metaregistrar\EPP\rrpproxyEppTrusteeRequest', 'Metaregistrar\EPP\eppUpdateDomainResponse');
