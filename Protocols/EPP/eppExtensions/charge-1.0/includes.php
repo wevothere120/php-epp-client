cb5a0c4407db67ee25aae2a56d078a4b65a2d321
Ewout de Graaf <ewout@metaregistrar.com>
2017-08-09 11:45:34 +0200

    Charge extension

--- Protocols/EPP/eppExtensions/charge-1.0/includes.php
+++ Protocols/EPP/eppExtensions/charge-1.0/includes.php
@@ -5,7 +5,7 @@
 $this->addExtension('charge','http://www.unitedtld.com/epp/charge-1.0');
 
 include_once(dirname(__FILE__) . '/eppRequests/chargeEppTransferDomainRequest.php');
-$this->addCommandResponse('Metaregistrar\EPP\chargeEppTransferDomainRequest', 'Metaregistrar\EPP\eppTransferDomainResponse');
+$this->addCommandResponse('Metaregistrar\EPP\chargeEppTransferDomainRequest', 'Metaregistrar\EPP\eppTransferResponse');
 
 include_once(dirname(__FILE__) . '/eppResponses/chargeEppCheckDomainResponse.php');
 $this->addCommandResponse('Metaregistrar\EPP\eppCheckDomainRequest', 'Metaregistrar\EPP\chargeEppCheckDomainResponse');
